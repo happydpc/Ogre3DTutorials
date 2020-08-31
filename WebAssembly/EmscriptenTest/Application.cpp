ff5a1b53117f532af02ad1d5de1192e64c1ec63d
Gustavo Branco <gbranco@netflix.com>
2020-08-30 21:45:09 -0700

    Rename project

---
+++ WebAssembly/EmscriptenTest/Application.cpp
@@ -0,0 +1,12 @@
+#include "Application.h"
+
+#include <iostream>
+
+
+void Application::Initialize() {
+    std::cout << "Initializing application." << std::endl;
+}
+
+void Application::SayHello() {
+    std::cout << "Hello!" << std::endl;
+}
